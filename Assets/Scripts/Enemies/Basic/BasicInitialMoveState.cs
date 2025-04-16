fb3166e3ee4827188933d9460251e62174695c15
IanPNorman <125617549+IanPNorman@users.noreply.github.com>
2025-04-15 19:15:06 -0700

    Asset Fixes, Finder class for AI, More Map Progress
    Woops accidently worked in main

--- Assets/Scripts/Enemies/Basic/BasicInitialMoveState.cs
+++ Assets/Scripts/Enemies/Basic/BasicInitialMoveState.cs
@@ -1,70 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine;
 
 public class BasicInitialMoveState : BasicState
 {
     public bool atDoor;
 
     public BasicState BasicBreakDoorState;
+    public NavMeshAgent agent;
 
-    public NavMeshAgent agent;  
-
-    public GameObject[] allDoors;
-
-    public float distanceTo;
-
-    public float lowestDistance;
-
-    public int arrNum;
-
-
+    private Finder finder;
+    private GameObject nearestDoor;
 
     public override BasicState RunCurrentState()
     {
         if (atDoor)
         {
-
             return BasicBreakDoorState;
         }
         else
         {
-            agent.SetDestination(nearestDoor());
-            CheckDistance();
+            nearestDoor = finder.FindNearestDoor(transform.parent.position);
+            if (nearestDoor != null)
+            {
+                agent.SetDestination(nearestDoor.transform.position);
+                CheckDistance(nearestDoor);
+            }
+
             return this;
         }
     }
 
     void Start()
     {
-        allDoors = GameObject.FindGameObjectsWithTag("Door"); // If map ever expands and more doors are added while the game is going this wont work 
         agent = GetComponentInParent<NavMeshAgent>();
+        finder = FindObjectOfType<Finder>(); 
     }
 
-    private void CheckDistance()
+    private void CheckDistance(GameObject target)
     {
-        if (lowestDistance < 2) {
-             atDoor = true;
-        }
-    }
-
-    private Vector3 nearestDoor()
-    {
-        
-        lowestDistance = Vector3.Distance(this.transform.parent.position, allDoors[0].transform.position);
-        for (int i = 0; i < allDoors.Length; i++)
+        float distance = Vector3.Distance(transform.parent.position, target.transform.position);
+        if (distance < 2)
         {
-            distanceTo = Vector3.Distance(this.transform.parent.position, allDoors[i].transform.position);
-            if (distanceTo <= lowestDistance)
-            {
-                lowestDistance = distanceTo;
-                arrNum = i;
-            }
+            atDoor = true;
         }
-        
-        return allDoors[arrNum].transform.position;
     }
-
-    
 }
